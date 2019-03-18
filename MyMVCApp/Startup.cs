4aea51a4c6a951d2248fd9d62b7269771e12003f
Jackie Pearce <jackiepearce83@hotmail.com>
2019-03-18 12:19:21 +0000

    Add project files.

---
+++ MyMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMVCApp.Startup))]
+namespace MyMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
