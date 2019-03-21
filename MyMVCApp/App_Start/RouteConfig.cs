29edfe5d122e7728f430b66f5f6595bc875b95f2
Jackie Pearce <jackiepearce83@hotmail.com>
2019-03-21 10:50:45 +0000

    Added new MapRoute in RouteConfig for URLs that start with /cuisine/

--- MyMVCApp/App_Start/RouteConfig.cs
+++ MyMVCApp/App_Start/RouteConfig.cs
@@ -13,6 +13,11 @@ namespace MyMVCApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Cuisine",
+                "cuisine/{name}",
+                new { controller = "Cuisine", action = "Search", name = "" }
+                );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
