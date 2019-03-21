f9b8d733e1fb80d71d3d84f922cd4b014dba2352
Jackie Pearce <jackiepearce83@hotmail.com>
2019-03-21 14:44:26 +0000

    Added another Search action method and added action selector attributes to specify when each will be invoked. Both reeturn a ContentResult

--- MyMVCApp/Controllers/CuisineController.cs
+++ MyMVCApp/Controllers/CuisineController.cs
@@ -8,11 +8,17 @@ namespace MyMVCApp.Controllers
 {
     public class CuisineController : Controller
     {
-        // GET: Cuisine
+        [HttpPost]
         public ActionResult Search(string name = "French")
         {
             var message = Server.HtmlEncode(name);
-            return Json(new { Message = message, Name = "Jackie" }, JsonRequestBehavior.AllowGet);
+            return Content(message);
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return Content("Search");
         }
     }
 }
