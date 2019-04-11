f12882ab727fd826b118455e52ee9727495beb16
Jackie Pearce <jackiepearce83@hotmail.com>
2019-04-11 16:49:13 +0100

    Removed MaxWords custom attribute

--- MyMVCApp/Models/RestaurantReview.cs
+++ MyMVCApp/Models/RestaurantReview.cs
@@ -5,36 +5,12 @@ using System.Linq;
 using System.Web;
 
 namespace MyMVCApp.Models
-{
-    public class MaxWordsAttribute : ValidationAttribute
-    {
-        private readonly int _maxWords;
-        public MaxWordsAttribute(int maxWords) : base("{0} has too many words")
-        {
-            _maxWords = maxWords;
-        }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var valueAsString = value.ToString();
-                if (valueAsString.Split(' ').Length > _maxWords)
-                {
-                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-                    return new ValidationResult(errorMessage);
-                }
-            }
-            return ValidationResult.Success;
-        }
-
-
-    }
-
+{   
     public class RestaurantReview
     {
-        public int Id { get; set; }  
-        
-        [Range(1,10)]
+        public int Id { get; set; }
+
+        [Range(1, 10)]
         [Required]
         public int Rating { get; set; }
 
@@ -42,9 +18,8 @@ namespace MyMVCApp.Models
         [Required]
         public string Body { get; set; }
 
-        [MaxWords(1)]
         [Display(Name = "User name")]
-        [DisplayFormat(NullDisplayText ="anonymous")]
+        [DisplayFormat(NullDisplayText = "anonymous")]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
 
