10053064808c014fe7a769652d56bd82ab87c1a9
Jackie Pearce <jackiepearce83@hotmail.com>
2019-04-11 16:48:06 +0100

    Wrote a custom validation attribute MaxWords to limit number of words in ReviewerName

--- MyMVCApp/Models/RestaurantReview.cs
+++ MyMVCApp/Models/RestaurantReview.cs
@@ -6,6 +6,30 @@ using System.Web;
 
 namespace MyMVCApp.Models
 {
+    public class MaxWordsAttribute : ValidationAttribute
+    {
+        private readonly int _maxWords;
+        public MaxWordsAttribute(int maxWords) : base("{0} has too many words")
+        {
+            _maxWords = maxWords;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Split(' ').Length > _maxWords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+
+    }
+
     public class RestaurantReview
     {
         public int Id { get; set; }  
@@ -17,6 +41,10 @@ namespace MyMVCApp.Models
         [StringLength(1024)]
         [Required]
         public string Body { get; set; }
+
+        [MaxWords(1)]
+        [Display(Name = "User name")]
+        [DisplayFormat(NullDisplayText ="anonymous")]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
 
