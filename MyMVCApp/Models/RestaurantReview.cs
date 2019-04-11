582bd36a3dd9fc45baa1b29c3dc54c0217c127f9
Jackie Pearce <jackiepearce83@hotmail.com>
2019-04-11 14:39:25 +0100

    Added vallidation annotation attributes to Ratig and body in RestaurantReview model

--- MyMVCApp/Models/RestaurantReview.cs
+++ MyMVCApp/Models/RestaurantReview.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,14 @@ namespace MyMVCApp.Models
 {
     public class RestaurantReview
     {
-        public int Id { get; set; }        
+        public int Id { get; set; }  
+        
+        [Range(1,10)]
+        [Required]
         public int Rating { get; set; }
+
+        [StringLength(1024)]
+        [Required]
         public string Body { get; set; }
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
