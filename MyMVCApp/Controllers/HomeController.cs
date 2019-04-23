6795aefd67bab4b7e152f25e3cf6fda65c17fed1
Jackie Pearce <jackiepearce83@hotmail.com>
2019-04-23 18:03:51 +0100

    Used the jQuery UI Autocomplete widget for the HomePage Search facility

--- MyMVCApp/Controllers/HomeController.cs
+++ MyMVCApp/Controllers/HomeController.cs
@@ -11,18 +11,22 @@ namespace MyMVCApp.Controllers
     {
         MyMVCAppDb _db = new MyMVCAppDb();
 
+        public ActionResult Autocomplete(string term)
+        {
+            var model =
+               _db.Restaurants
+               .Where(r => r.Name.StartsWith(term))
+               .Take(10)
+               .Select(r => new
+               {
+                   label = r.Name
+               });
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Index(string searchTerm = null)
         {
-            //var model = from r in _db.Restaurants
-            //            orderby r.Reviews.Average(review => review.Rating) descending
-            //            select new RestaurantListViewModel
-            //            {
-            //                Id = r.Id,
-            //                Name = r.Name,
-            //                City = r.City,
-            //                Country = r.Country,
-            //                CountOfReviews = r.Reviews.Count()
-            //            };
+            
             var model =
                 _db.Restaurants
                 .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
--- MyMVCApp/Scripts/otf.js
+++ MyMVCApp/Scripts/otf.js
@@ -17,5 +17,23 @@ $(function () {
         return false;
     };
 
+    var submitAutocompleteForm = function (event, ui) {
+        var $input = $(this);
+        $input.val(ui.item.label);
+
+        var $form = $input.parents("form:first");
+        $form.submit();
+    };
+
+    var createAutoComplete = function () {
+        var $input = $(this);
+        var options = {
+            source: $input.attr("data-otf-autocomplete"),
+            select: submitAutocompleteForm
+        };
+        $input.autocomplete(options);
+    };
+
     $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+    $("input[data-otf-autocomplete]").each(createAutoComplete);
 });
