55773251140c7715b00c4ee39b371b29bf49ad71
Jackie Pearce <jackiepearce83@hotmail.com>
2019-04-24 16:28:59 +0100

    Made paging of restaurantList asynchronous

--- MyMVCApp/Scripts/otf.js
+++ MyMVCApp/Scripts/otf.js
@@ -34,6 +34,20 @@ $(function () {
         $input.autocomplete(options);
     };
 
+    var getPage = function () {
+        var $a = $(this);
+        var options = {
+            url: $a.attr("href"),
+            type: "get"
+        };
+        $.ajax(options).done(function (data) {
+            var $target = $($a.parents("div.pagedList").attr("data-otf-target"));
+            $target.replaceWith(data);
+        });
+        return false;
+    };
+
     $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
     $("input[data-otf-autocomplete]").each(createAutoComplete);
+    $(".body-section").on("click", ".pagedList a", getPage);
 });
